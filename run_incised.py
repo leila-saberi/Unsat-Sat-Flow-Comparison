b0f25771de695a3be232f86817a640a8199f0189
Leila Saberi <86313350+leila-saberi@users.noreply.github.com>
2023-04-17 13:27:12 -0500

    Calc zone budget and run NWT models

--- run_incised.py
+++ run_incised.py
@@ -10,8 +10,8 @@ import pandas as pd
 # import pyemu
 import subprocess
 
-mf6 = False
-base_dir = 'test1_newBCs_Sce2_refined'#'mf6_sce2_refined'
+mf6 = True
+base_dir = 'mf6_sce2_refined'#'test1_newBCs_Sce2_refined'#'mf6_sce2_refined'
 
 if mf6 == True:
     modelname = 'incised'
@@ -36,9 +36,9 @@ if mf6 == True:
     cbb_file = os.path.join(base_dir, modelname + ".cbb")
     zon = np.ones(bas_shape, dtype=int)
     # upgradien zone
-    zon[:, 38:77, 1] = 4
+    zon[:, 27:54, 1] = 4
     # River zone:
-    zon[:, 38:77, -1] = 2
+    zon[:, 27:54, -1] = 2
     # Pond zone:
     zon_rows, zon_cols = np.nonzero(bas[0, :, :])
     # zon[0,np.nonzero(bas.ibound[0,:,:])] = 3
@@ -51,23 +51,31 @@ if mf6 == True:
     zonbud = m.output.zonebudget(zon)
     zonbud.change_model_ws(ws)
     zonbud.write_input()
-    zbud6_exe = os.path.join(base_dir, 'zbud6.exe')
-    zbud_nam = os.path.join(base_dir, 'zonebud.zbnam')
+    zbud6_exe = os.path.join(os.getcwd(), base_dir)
+    # zbud_nam = os.path.join(base_dir, 'zonebud.zbnam')
 
-    # subprocess.call([f'{zbud6_exe}', f'{zbud_nam}'])
+    # args = f'{zbud6_exe}', f'{zbud_nam}'
 
-    zb = zonbud.get_budget()
-    df = zonbud.get_dataframes().reset_index()
-    flow_DNG = df['ZONE_2'][5] * 28.3168  # SS rate into river (L/day)
-    flow_UPG = df['ZONE_4'][5] * 28.3168  # SS rate into river (L/day)
-    flow_pond = df['ZONE_3'][5] * 28.3168  # SS effluent rate from pond (L/day)
-    # flow_well = 0.5 * (df['ZONE_1'][5] + df['ZONE_4'][2]) * 28.3168  # SS flow through well (L/day)
-    print(df.to_string())
-    print('Flow into river: ' + str(flow_DNG) + ' L/day.')
-    print('Flow from pond: ' + str(flow_pond) + ' L/day.')
-    print('Flow through UPG: ' + str(flow_UPG) + ' L/day.')
-    print('Percent: ' + str(flow_pond / flow_DNG * 100.0))
-    exit()
+    # zbud6_exe = r'C:/Users/lsaberi/GitHub/Unsat-Sat-Flow-Comparison/mf6_sce0_refined/zonebudget.bat'
+
+    p = subprocess.Popen(["zonebudget.bat"], shell=True, cwd=zbud6_exe)
+
+    stdout, stderr = p.communicate()
+    print(p.returncode)  # is 0 if success
+
+    if p.returncode == 0:
+        zb = zonbud.get_budget()
+        df = zonbud.get_dataframes().reset_index()
+        flow_DNG = df['ZONE_2'][5] * 28.3168  # SS rate into river (L/day)
+        flow_UPG = df['ZONE_4'][5] * 28.3168  # SS rate into river (L/day)
+        flow_pond = df['ZONE_3'][5] * 28.3168  # SS effluent rate from pond (L/day)
+        # flow_well = 0.5 * (df['ZONE_1'][5] + df['ZONE_4'][2]) * 28.3168  # SS flow through well (L/day)
+        print(df.to_string())
+        print('Flow into river: ' + str(flow_DNG) + ' L/day.')
+        print('Flow from pond: ' + str(flow_pond) + ' L/day.')
+        print('Flow through UPG: ' + str(flow_UPG) + ' L/day.')
+        print('Percent: ' + str(flow_pond / flow_DNG * 100.0))
+        exit()
 
 else:
     c_riv, c_well, flows = ccr.incised_vertical.model_output(base_dir=base_dir)
